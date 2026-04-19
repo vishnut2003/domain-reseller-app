4038a5046b93ccb73b1910def62e2558fb3621f7
vishnu <iam.vishnu.bhaskar@gmail.com>
2026-04-19 13:44:25 +0530

    Completed Home Hero Section;

---
+++ app/(home)/(components)/hero-section/index.tsx
@@ -0,0 +1,43 @@
+import { Button } from "@/components/ui/button";
+import DefaultSection from "@/layouts/default-section";
+import { RiSearchLine } from "@remixicon/react";
+import BgImage from "./assets/hero-bg-image.jpg";
+
+export default async function HeroSection() {
+    return (
+        <DefaultSection
+            className="flex items-stretch min-h-150 gap-10"
+        >
+            <div
+                className="relative w-full rounded-3xl flex gap-5 items-center justify-center bg-cover bg-center overflow-hidden"
+                style={{backgroundImage: `url(${BgImage.src})`}}
+            >
+
+                <div
+                    className="absolute top-0 left-0 w-full h-full bg-linear-to-br from-theme-primary to-theme-dark opacity-80 z-0"
+                />
+
+                <div
+                    className="flex gap-5 flex-col items-center justify-center z-10"
+                >
+                    <h1
+                        className="text-4xl leading-10 max-w-140 text-center text-white font-black"
+                    >Find the Perfect Domain Name for Your Next Big Idea</h1>
+                    <p
+                        className="max-w-150 text-center text-white/70"
+                    >Search and register domains instantly at the best prices. Launch your online presence with ease—fast, secure, and hassle-free.</p>
+
+                    <Button
+                        variant={"default"}
+                        className="py-6 px-10"
+                        size={"lg"}
+                    >
+                        <RiSearchLine />
+                        <p>Search Domain</p>
+                    </Button>
+                </div>
+
+            </div>
+        </DefaultSection>
+    )
+}
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,10 @@
 import { BasicLayout } from "@/layouts/basic-layout";
+import HeroSection from "./(components)/hero-section";
 
 export default function Home() {
   return (
     <BasicLayout>
-      
+      <HeroSection />
     </BasicLayout>
   );
 }
